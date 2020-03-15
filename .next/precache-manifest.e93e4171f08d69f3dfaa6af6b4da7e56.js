a244ba73458adf84325acb622d1fdaf5eaec600a
wuwei <deploy@travis-ci.org>
2020-03-15 08:23:07 +0000

    Deploy wuweikd/efox-lang-hook to github.com/wuweikd/efox-lang-hook.git:DEPLOY-PROD

---
+++ .next/precache-manifest.e93e4171f08d69f3dfaa6af6b4da7e56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112d2ff00a49affb7dbfe5ce1e228fd8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5132f68322bc66211e40",
+    "url": "/static/js/2.38286f51.chunk.js"
+  },
+  {
+    "revision": "6b1c9aa1f24077e4bfd4b51db2988c14",
+    "url": "/static/js/2.38286f51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c147b303bd7b9625a8f",
+    "url": "/static/js/main.6f32a378.chunk.js"
+  },
+  {
+    "revision": "87450c52ad4fb1625154",
+    "url": "/static/js/runtime-main.7646b05b.js"
+  }
+]);
